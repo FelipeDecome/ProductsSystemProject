a78b3519465cfc7dc2b498a5dc0d3e337311b6ef
FelipeDecome <felipe.decome@hotmail.com>
2019-12-10 16:45:03 -0300

    Update index.php

--- tests/index.php
+++ tests/index.php
@@ -3,13 +3,11 @@
 //Item Teste
 $data = [
 
-	'name' => 'bola',
-	'type' => 'brinquedos',
-	'description' => 'caixa com 120 peças.',
-	'reference' => '4007',
-	'on_stock' => true,
-	'url_img' => 'public/img/img_01'
+    'name' => 'bola',
+    'type' => 'brinquedos',
+    'description' => 'caixa com 120 peças.',
+    'reference' => '4007',
+    'on_stock' => true,
+    'url_img' => 'public/img/img_01',
 
 ];
-
-
